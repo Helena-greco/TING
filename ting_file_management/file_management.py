fef6c315edcff0acda9c1813526433151278db25
Helena <nenafgreco@hotmail.com>
2022-07-18 19:26:53 -0400

    corrigindo erros

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,9 +1,10 @@
 import sys
 
+
 def txt_importer(path_file):
-    if not ".txt" in path_file:
+    if ".txt" not in path_file:
         sys.stderr.write(f"Formato inválido\n")
-    
+
     try:
         with open(path_file, "r", encoding="utf-8") as file:
             text_file = file.read().splitlines()
@@ -12,5 +13,5 @@ def txt_importer(path_file):
     except FileNotFoundError:
         sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
 
-# Ref: https://docs.python.org/3/library/stdtypes.html#functions - str.splitlines()
+# Ref: https://docs.python.org/3/library/stdtypes.html#functions splitlines()
 # Ref: https://www.askpython.com/python/python-stdin-stdout-stderr
