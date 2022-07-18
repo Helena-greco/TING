923863d93a07745d96bba4a6f7fab01f4380d611
Helena <nenafgreco@hotmail.com>
2022-07-18 19:29:59 -0400

    corrigindo erros

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,7 +3,7 @@ import sys
 
 def txt_importer(path_file):
     if ".txt" not in path_file:
-        sys.stderr.write(f"Formato inválido\n")
+        sys.stderr.write("Formato inválido\n")
 
     try:
         with open(path_file, "r", encoding="utf-8") as file:
