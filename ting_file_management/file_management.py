9b0feb87c2b1c8530855e7d36cf215f465d36364
Helena <nenafgreco@hotmail.com>
2022-07-18 19:23:28 -0400

    requisito 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,16 @@
+import sys
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not ".txt" in path_file:
+        sys.stderr.write(f"Formato inválido\n")
+    
+    try:
+        with open(path_file, "r", encoding="utf-8") as file:
+            text_file = file.read().splitlines()
+            return text_file
+
+    except FileNotFoundError:
+        sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+
+# Ref: https://docs.python.org/3/library/stdtypes.html#functions - str.splitlines()
+# Ref: https://www.askpython.com/python/python-stdin-stdout-stderr
