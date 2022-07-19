81c4022368bf39382cf2f2936b19c1c8ec97ad83
Helena <nenafgreco@hotmail.com>
2022-07-19 14:36:51 -0400

    processamento dos dados a partir do path

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,6 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.file_management import txt_importer
+import sys
 
+
+def process(path_file, instance):
+    list_of_paths = list()
+    list_of_files = dict()
+    index_instance = range(len(instance))
+
+    for index in index_instance:
+        list_of_paths.append(instance.search(index))
+    
+    if path_file not in list_of_paths:
+        file = txt_importer(path_file)
+
+        list_of_files["nome_do_arquivo"] = path_file
+        list_of_files["qtd_linhas"] = len(file)
+        list_of_files["linhas_do_arquivo"] = file
+
+    instance.enqueue(list_of_files)
+    sys.stdout.write(str(list_of_files))
 
 def remove(instance):
     """Aqui irá sua implementação"""
