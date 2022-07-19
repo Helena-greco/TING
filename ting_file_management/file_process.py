3f2989dd723a169139f0cf3107b8f0170a191921
Helena <nenafgreco@hotmail.com>
2022-07-19 15:39:24 -0400

    corrigindo função e referenciando

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -4,20 +4,24 @@ import sys
 
 def process(path_file, instance):
     list_of_paths = list()
-    list_of_files = dict()
 
-    for index in range(len(instance)):
-        list_of_paths.append(instance.search(index))
+#confirmando se existe instancia da fila
+# consultei o repo https://github.com/tryber/sd-014-b-project-ting/pull/7
+# para a solução de ignorar path já existente
+    if len(instance) > 0:
+        for index in range(len(instance)):
+            list_of_paths.append(instance.search(index)["nome_do_arquivo"])
 
     if path_file not in list_of_paths:
         file = txt_importer(path_file)
 
+        list_of_files = dict()
         list_of_files["nome_do_arquivo"] = path_file
         list_of_files["qtd_linhas"] = len(file)
         list_of_files["linhas_do_arquivo"] = file
 
-    instance.enqueue(list_of_files)
-    sys.stdout.write(str(list_of_files))
+        instance.enqueue(list_of_files)
+        sys.stdout.write(str(list_of_files))
 
 
 def remove(instance):
