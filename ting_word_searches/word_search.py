e1444c9fd5e0a76c03c2bdb40fb0e538d98ddc18
Helena <nenafgreco@hotmail.com>
2022-07-19 16:39:34 -0400

    corrigindo lint

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -6,10 +6,11 @@ def exists_word(word, instance):
         total_lines = file["linhas_do_arquivo"]
         line_counting = list()
 
-# Ref: https://stackoverflow.com/questions/319426/how-do-i-do-a-case-insensitive-string-comparison
+# Ref: https://stackoverflow.com/questions/319426/
+# how-do-i-do-a-case-insensitive-string-comparison
         for line in range(len(total_lines)):
             if word.lower() in total_lines[line].lower():
-                line_counting.append({ "linha": line + 1 })
+                line_counting.append({"linha": line + 1})
 
         if len(line_counting) > 0:
             list_of_searchs.append({
